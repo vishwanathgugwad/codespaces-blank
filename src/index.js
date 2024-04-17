032d24950f4c684eebd7aa54aebf2ef4d9da586d
Vishwanath <vishwanath.gugwad@gmail.com>
2024-04-17 15:07:43 +0000

    mongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-const express = require('express');
-require('dotenv').config()
-const port = 4000
-const app = express()
 
 
-app.get('/',(req , res) => {
-    res.send("hello world")
-})
-app.get('/india',(req , res) => {
-    res.send("hello world india")
-})
 
-app.listen(process.env.PORT , () => {
-    console.log(`listing on port ${process.env.PORT}`)
-})
+import dotenv from "dotenv";
+dotenv.config({path:'./env'})
+
+import connectDB from "./db/index.js";
+
+connectDB()
